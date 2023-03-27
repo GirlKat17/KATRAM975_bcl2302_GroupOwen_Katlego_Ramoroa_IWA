e7e681c34d561335864221748691f3f3af0253ce
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-03-27 16:29:53 +0200

    updated
    iwa1

---
+++ Interactive-web-apps/IWA1/Scripts.js
@@ -0,0 +1,17 @@
+const number= document.querySelector('[data-key="number"]')
+const substract= document.querySelector('[data-key="subtract"]')
+const add= document.querySelector('[data-key="add"]')
+
+
+
+const substractHandler = () => { log(...data:any[]): void
+
+}
+
+
+const addHandler = () => {
+
+}
+
+substract.addEventListener('click', substractHandler)
+add.addEventListener('click', addHandler)
