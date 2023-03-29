8f9e5f4385b1e97abe4aea07b8c17a31fee9e92f
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-03-29 15:14:30 +0200

    bcl2302_grOwen_Katlego_Ramoroa_IWA03
    IWA3

---
+++ IWA3/Challenge 2/johannes.js
@@ -0,0 +1,8 @@
+
+export const firstname = "Johannes";
+export const surname = "Potgieter";
+export  const role = "Intern";
+
+
+var display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#johannes').innerText = display
---
+++ IWA3/Challenge 2/scripts.js
@@ -0,0 +1,11 @@
+
+
+import {role as role1} from"./johannes.js";
+import{role as role2} from"./nwabisa.js";
+import{role as role3} from"./alex.js" ;
+
+
+
+
+console.log('Roles:', role2,role1, role3)
+
