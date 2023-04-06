7bce8d4cd94322ea46d603e6722f5e96ee11c166
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-04-06 13:25:54 +0200

    bcl2302_grOwen_Katlego_Ramoroa_IWA8
    IWA8

---
+++ IWA8/Challenge 1.js
@@ -0,0 +1,49 @@
+const leoName = 'Leo Musvaire'
+const leoNumber = '2'
+const leoStreet = 'Church St.'
+const leoPostal = '3105'
+const leoBalance = '-10'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+const sarahNumber = '13'
+const sarahStreet = 'William Close'
+const sarahPostal = '0310'
+
+// Only change below this line
+
+const leo ={
+	CustomerName :leoName,
+	Balance :leoBalance,
+	AccessId:'47afb389-8014-4d0b-aff3-e40203d2107f',
+	Age : 24,
+}
+
+	 Address ={
+		Number :leoNumber,
+		Street : leoStreet,
+		Postal_Code :leoPostal,
+	}
+  
+
+
+
+const sarah ={
+CustomerName : sarahName + sarahSurname,
+Age :62,
+AccessId :'6b279ae5-5657-4240-80e9-23f6b635f7a8',
+Balance : sarahBalance,
+}
+	Address = {
+		Number : sarahNumber,
+		StreetName : sarahStreet,
+		postal_Code :sarahPostal,
+	}
+
+
+    console.log(leo, {Address})
+    console.log(sarah, {Address})
+
+//console.log(leo, leo[address][postal_code])
+//console.log(sarah, sarah[address][postal-code])
