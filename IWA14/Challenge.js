792e264d5aca61d5fdf772748cdc0d8ec88f9538
Katlego Ramoroa <ramoroakatlego@gmail.com>
2023-04-18 14:34:53 +0200

    bcl2302_grOwen_Katlego_Ramoroa_IWA14
    IWA14

--- IWA14/Challenge.js
+++ IWA14/Challenge.js
@@ -1,14 +1,16 @@
-firstName = 'John';
-age = 35;
-hobby = 'Coding';
+const firstName = 'John';
+const age = 35;
+const hobbyKat = 'Coding';
 
-const logTwice = () => {
-  console(parameter)
-  console(parameter)
+const logTwice = (parameter) => {
+  console.log(parameter)
+  console.log(parameter)
 }
 
 function hobby () {
-  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
-}
 
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobbyKat}!`);
+ 
+}
 hobby()
+
